7c5994c8f96289d590b1c7a828a56ad163552b46
Kristian Freeman <kristian@kristianfreeman.com>
2013-09-01 16:16:49 -0700

    basic processing of podcast in job

--- app/jobs/generate_podcast.rb
+++ app/jobs/generate_podcast.rb
@@ -4,7 +4,27 @@ module GeneratePodcast
   def self.perform()
     @podcasts = Podcast.all
     @podcasts.each do |podcast|
-      puts podcast.name
+      process_podcast(podcast)
     end
+  end
+
+  def process_podcast(podcast)
+    # do the stuff
+    name = podcast.name
+    description = podcast.description
+    cover = podcast.cover
+    podcast.episodes.each do |episode|
+      process_episode(episode)
+    end
+    puts "processed #{ podcast.name }"
+  end
+
+  def process_episode(episode)
+    name = episode.name
+    description = episode.description
+    cover = episode.cover
+    link = episode.link
+    sc_id = episode.sc_id
+    puts "processed #{ episode.name }"
   end
 end
