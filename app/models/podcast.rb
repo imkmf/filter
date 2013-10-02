42c91277b28e5645b7aa9ef0714caa86b048ebd5
Kristian Freeman <kristian@kristianfreeman.com>
2013-10-01 21:03:20 -0700

    Fix adding of categories to podcasts

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -4,6 +4,7 @@ class Podcast < ActiveRecord::Base
   after_initialize :set_name
   has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }
   has_and_belongs_to_many :categories
+  accepts_nested_attributes_for :categories
 
   def set_name
     if user
