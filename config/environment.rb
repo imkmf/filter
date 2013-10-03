26c2300d053990f0c2567e83aae630588510c3d2
Kristian Freeman <kristian@kristianfreeman.com>
2013-10-02 18:33:27 -0700

    Add Mandrill

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Filter::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: Figaro.env.mandrill_user,
+  password: Figaro.env.mandrill_pass,
+  domain: 'usefilter.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
+
