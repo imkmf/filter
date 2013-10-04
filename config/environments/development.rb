9a09f7f94110957fdaf6274feb633759c36af36a
Kristian Freeman <kristian@kristianfreeman.com>
2013-10-03 18:36:01 -0700

    Add Letter Opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Filter::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # Letter Opener!
+  config.action_mailer.delivery_method = :letter_opener
 end
