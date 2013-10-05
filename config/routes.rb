c78f8534f49a062a1fbdbba9dc63479df66f8415
Kristian Freeman <kristian@kristianfreeman.com>
2013-10-05 14:27:07 -0700

    Protect resque!

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Filter::Application.routes.draw do
   post "/users/remove_card", to: "subscriptions#remove_card", as: "remove_card"
   post "/subscriptions/reactivate", to: "subscriptions#reactivate", as: "reactivate_subscription"
   post "episodes/blacklist", to: "episodes#blacklist", as: "blacklist_episode"
-  mount Resque::Server, :at => "/resque"
+  mount Resque::Server, :at => "/resque" if Rails.env.development?
 end
