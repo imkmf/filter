1474c7096f23be5e6bab14c7fab4fdc2847d16a0
Kristian Freeman <kristian@kristianfreeman.com>
2013-10-18 19:15:58 -0700

    Production keys

---
+++ config/initializers/stripe_event.rb
@@ -0,0 +1,22 @@
+if Rails.env.development?
+  Stripe.api_key = Figaro.env.stripe_key
+else
+  Stripe.api_key = Figaro.env.stripe_key_production
+end
+
+StripeEvent.setup do
+  subscribe 'charge.failed' do |event|
+    # Define subscriber behavior based on the event object
+    event.class #=> Stripe::Event
+    event.type  #=> "charge.failed"
+    event.data  #=> { ... }
+  end
+
+  subscribe 'customer.created', 'customer.updated' do |event|
+    # Handle multiple event types
+  end
+
+  subscribe do |event|
+    # Handle all event types - logging, etc.
+  end
+end
